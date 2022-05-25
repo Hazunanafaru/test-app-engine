c28721c2651d2987254c9d1bb6ffbbff6e889156
husnizuhdi <husni.zuhdi@tokopedia.com>
2022-05-25 16:01:17 +0700

    Refactor to use gin instead of http

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/husnizuhdi/test-app-engine
 
 go 1.15
 
-require cloud.google.com/go/storage v1.22.1 // indirect
+require (
+	cloud.google.com/go/storage v1.22.1 // indirect
+	github.com/gin-gonic/gin v1.7.7 // indirect
+)
--- main.go
+++ main.go
@@ -1,34 +1,47 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
 	"os"
+
+	// "cloud.google.com/go/storage"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
-	}
-
-	log.Printf("Listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-
 	name := os.Getenv("NAME")
-	hello := "Hello " + name + " and the World"
-	fmt.Fprint(w, hello)
+	hello := "Hello " + name + " and the World!"
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": hello,
+		})
+	})
+
+	r.Run()
+
+	// http.HandleFunc("/", indexHandler)
+
+	// port := os.Getenv("PORT")
+	// if port == "" {
+	// 	port = "8080"
+	// 	log.Printf("Defaulting to port %s", port)
+	// }
+
+	// log.Printf("Listening on port %s", port)
+	// if err := http.ListenAndServe(":"+port, nil); err != nil {
+	// 	log.Fatal(err)
+	// }
 }
+
+// func indexHandler(w http.ResponseWriter, r *http.Request) {
+// 	if r.URL.Path != "/" {
+// 		http.NotFound(w, r)
+// 		return
+// 	}
+
+// 	name := os.Getenv("NAME")
+// 	hello := "Hello " + name + " and the World"
+// 	fmt.Fprint(w, hello)
+// }
